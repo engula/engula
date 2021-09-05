3394b1956232f44c38f0bc1c17df423c20c24439
Huachao Huang <huachao.huang@gmail.com>
2021-09-05 19:40:17 +0800

    Add RemoteFileSystem

--- engula/build.rs
+++ engula/build.rs
@@ -4,6 +4,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             "src/format/format.proto",
             "src/journal/journal.proto",
             "src/manifest/manifest.proto",
+            "src/file_system/file_system.proto",
         ],
         &["src"],
     )?;
---
+++ engula/src/file_system/remote_file_system.rs
@@ -0,0 +1,102 @@
+use async_trait::async_trait;
+use tokio::sync::Mutex;
+use tonic::transport::Channel;
+use tonic::Request;
+
+use super::{
+    file_system_client, AccessMode, FileSystem, OpenRequest, RandomAccessReader, ReadRequest,
+    RemoveRequest, SequentialWriter, SyncRequest, WriteRequest,
+};
+use crate::error::Result;
+
+type FileSystemClient = file_system_client::FileSystemClient<Channel>;
+
+pub struct RemoteFileSystem {
+    client: Mutex<FileSystemClient>,
+}
+
+impl RemoteFileSystem {
+    async fn open_file(&self, fname: &str, mode: AccessMode) -> Result<RemoteFile> {
+        let input = OpenRequest {
+            file_name: fname.to_owned(),
+            access_mode: mode as i32,
+        };
+        let request = Request::new(input);
+        let mut client = self.client.lock().await;
+        let response = client.open(request).await?;
+        let output = response.into_inner();
+        Ok(RemoteFile::new(output.fd, client.clone()))
+    }
+}
+
+#[async_trait]
+impl FileSystem for RemoteFileSystem {
+    async fn new_sequential_writer(&self, fname: &str) -> Result<Box<dyn SequentialWriter>> {
+        let file = self.open_file(fname, AccessMode::Write).await?;
+        Ok(Box::new(file))
+    }
+
+    async fn new_random_access_reader(&self, fname: &str) -> Result<Box<dyn RandomAccessReader>> {
+        let file = self.open_file(fname, AccessMode::Read).await?;
+        Ok(Box::new(file))
+    }
+
+    async fn remove_file(&self, fname: &str) -> Result<()> {
+        let input = RemoveRequest {
+            file_name: fname.to_owned(),
+        };
+        let request = Request::new(input);
+        let mut client = self.client.lock().await;
+        client.remove(request).await?;
+        Ok(())
+    }
+}
+
+struct RemoteFile {
+    fd: u64,
+    client: FileSystemClient,
+}
+
+impl RemoteFile {
+    fn new(fd: u64, client: FileSystemClient) -> RemoteFile {
+        RemoteFile { fd, client }
+    }
+}
+
+#[async_trait]
+impl SequentialWriter for RemoteFile {
+    async fn write(&mut self, buf: &[u8]) -> Result<()> {
+        let input = WriteRequest {
+            fd: self.fd,
+            data: buf.to_owned(),
+        };
+        let request = Request::new(input);
+        let mut client = self.client.clone();
+        client.write(request).await?;
+        Ok(())
+    }
+
+    async fn sync(&mut self) -> Result<()> {
+        let input = SyncRequest { fd: self.fd };
+        let request = Request::new(input);
+        let mut client = self.client.clone();
+        client.sync(request).await?;
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl RandomAccessReader for RemoteFile {
+    async fn read_at(&self, offset: u64, size: u64) -> Result<Vec<u8>> {
+        let input = ReadRequest {
+            fd: self.fd,
+            offset,
+            size,
+        };
+        let request = Request::new(input);
+        let mut client = self.client.clone();
+        let response = client.read(request).await?;
+        let output = response.into_inner();
+        Ok(output.data)
+    }
+}
