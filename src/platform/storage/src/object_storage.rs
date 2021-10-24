027b59acc89f8970eae7daab2da8acfa8288871e
Richard <huachao.huang@gmail.com>
2021-10-25 01:42:29 +0800

    platform/storage: add some basic traits (#49)

--- src/platform/storage/src/object_storage.rs
+++ src/platform/storage/src/object_storage.rs
@@ -12,6 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-fn main() {
-    println!("Hello, world!");
+use async_trait::async_trait;
+
+use crate::{bucket_handle::BucketHandle, error::StorageResult};
+
+#[async_trait]
+pub trait ObjectStorage {
+    fn bucket(&self, name: &str) -> Box<dyn BucketHandle>;
+
+    async fn create_bucket(&self, name: &str) -> StorageResult<()>;
+
+    async fn delete_bucket(&self, name: &str) -> StorageResult<()>;
 }
