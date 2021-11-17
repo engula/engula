bd69f2e09a1a39e35afa9b1d6d17820ec452433f
zojw <93670789+zojw@users.noreply.github.com>
2021-11-17 23:11:18 +0800

    platform: add aws s3 storage

---
+++ src/platform/aws/src/s3/object.rs
@@ -0,0 +1,57 @@
+// Copyright 2021 The Engula Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use aws_sdk_s3::Client;
+use bytes::Buf;
+use storage::{async_trait, Object};
+
+use super::error::{Error, Result};
+
+pub struct S3Object {
+    client: Client,
+    bucket_name: String,
+    key: String,
+}
+
+impl S3Object {
+    pub fn new(client: Client, bucket_name: String, key: String) -> Self {
+        Self {
+            client,
+            bucket_name,
+            key,
+        }
+    }
+}
+
+#[async_trait]
+impl Object for S3Object {
+    type Error = Error;
+
+    async fn read_at(&self, buf: &mut [u8], offset: usize) -> Result<usize> {
+        let size = buf.len();
+        let range = format!("bytes={}-{}", offset, offset + size - 1);
+        let output = self
+            .client
+            .get_object()
+            .bucket(self.bucket_name.to_owned())
+            .key(self.key.to_owned())
+            .range(range)
+            .send()
+            .await?;
+
+        let mut bytes = output.body.collect().await?;
+        bytes.copy_to_slice(buf);
+        Ok(buf.len())
+    }
+}
---
+++ src/platform/aws/src/s3/storage.rs
@@ -0,0 +1,101 @@
+// Copyright 2021 The Engula Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use aws_sdk_s3::{
+    model::{BucketLocationConstraint, CreateBucketConfiguration, PublicAccessBlockConfiguration},
+    Client, Config, Credentials, Region,
+};
+use storage::{async_trait, Storage};
+
+use super::{bucket::S3Bucket, error::Result, object::S3Object};
+
+pub struct S3Storage {
+    client: Client,
+    region: String,
+}
+
+impl S3Storage {
+    pub fn new(
+        region: impl Into<String>,
+        access_key: impl Into<String>,
+        secret_key: impl Into<String>,
+    ) -> Self {
+        let credentials = Credentials::from_keys(access_key, secret_key, None);
+        let region = region.into();
+        let client = Client::from_conf(
+            Config::builder()
+                .region(Some(Region::new(region.to_owned())))
+                .credentials_provider(credentials)
+                .build(),
+        );
+        Self::from_client(client, region)
+    }
+
+    pub fn from_client(client: Client, region: impl Into<String>) -> Self {
+        let region = region.into();
+        Self { client, region }
+    }
+}
+
+#[async_trait]
+impl Storage<S3Object, S3Bucket> for S3Storage {
+    async fn bucket(&self, name: &str) -> Result<S3Bucket> {
+        self.client
+            .head_bucket()
+            .bucket(name.to_owned())
+            .send()
+            .await?;
+
+        Ok(S3Bucket::new(self.client.clone(), name))
+    }
+
+    async fn create_bucket(&self, name: &str) -> Result<S3Bucket> {
+        let region: &str = &self.region;
+        let location = BucketLocationConstraint::from(region);
+        let config = CreateBucketConfiguration::builder()
+            .location_constraint(location)
+            .build();
+        self.client
+            .create_bucket()
+            .bucket(name.to_owned())
+            .create_bucket_configuration(config)
+            .send()
+            .await?;
+
+        self.client
+            .put_public_access_block()
+            .bucket(name.to_owned())
+            .public_access_block_configuration(
+                PublicAccessBlockConfiguration::builder()
+                    .restrict_public_buckets(true)
+                    .block_public_policy(true)
+                    .ignore_public_acls(true)
+                    .block_public_acls(true)
+                    .build(),
+            )
+            .send()
+            .await?;
+
+        Ok(S3Bucket::new(self.client.clone(), name))
+    }
+
+    async fn delete_bucket(&self, name: &str) -> Result<()> {
+        self.client
+            .delete_bucket()
+            .bucket(name.to_owned())
+            .send()
+            .await?;
+        Ok(())
+    }
+}
