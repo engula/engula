5c06ecc3fd8212d26c22250479af43bb6dd9ff81
Richard <huachao.huang@gmail.com>
2021-11-18 22:53:19 +0800

    storage: implement grpc server (#115)

--- src/storage/src/grpc/mod.rs
+++ src/storage/src/grpc/mod.rs
@@ -17,11 +17,51 @@ mod client;
 mod error;
 mod object;
 mod proto;
+mod server;
 mod storage;
 
 pub use self::{
     bucket::{RemoteBucket, RemoteObjectUploader},
+    client::Client,
     error::{Error, Result},
     object::RemoteObject,
+    server::Server,
     storage::RemoteStorage,
 };
+
+#[cfg(test)]
+mod tests {
+    use tokio::net::TcpListener;
+    use tokio_stream::wrappers::TcpListenerStream;
+
+    use super::*;
+    use crate::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test() -> std::result::Result<(), Box<dyn std::error::Error>> {
+        let listener = TcpListener::bind("127.0.0.1:0").await?;
+        let local_addr = listener.local_addr()?;
+        tokio::task::spawn(async move {
+            let server = Server::new(mem::MemStorage::default());
+            tonic::transport::Server::builder()
+                .add_service(server.into_service())
+                .serve_with_incoming(TcpListenerStream::new(listener))
+                .await
+                .unwrap();
+        });
+
+        let url = format!("http://{}", local_addr);
+        let storage = RemoteStorage::connect(&url).await?;
+        let bucket = storage.create_bucket("bucket").await?;
+        let mut up = bucket.upload_object("object").await?;
+        let buf = vec![0, 1, 2];
+        up.write(&buf).await?;
+        let len = up.finish().await?;
+        assert_eq!(len, buf.len());
+        let object = bucket.object("object").await?;
+        let mut got = vec![0; buf.len()];
+        object.read_at(&mut got, 0).await?;
+        assert_eq!(got, buf);
+        Ok(())
+    }
+}
