6074dce9bdf8dec704b04ba39beb8ff3129921dc
Richard <huachao.huang@gmail.com>
2021-12-05 11:23:19 +0800

    Add some descriptions

--- src/engine/hash/src/lib.rs
+++ src/engine/hash/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! An Engula engine that provides simple key-value data storage.
+
 mod engine;
 mod error;
 mod format;
