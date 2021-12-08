628ef1b2c8a4b9653f26360ce98dae6008a3a6fc
p4tr1ck <patricknicholas@foxmail.com>
2021-12-08 21:35:41 +0800

    remove duplicated test code

--- src/kernel/src/grpc/mod.rs
+++ src/kernel/src/grpc/mod.rs
@@ -22,13 +22,8 @@ pub use self::{client::Client, kernel::Kernel, server::Server};
 
 #[cfg(test)]
 mod tests {
-    use engula_journal::{grpc::Server as JournalServer, mem::Journal as MemJournal, Journal};
-    use engula_storage::{grpc::Server as StorageServer, mem::Storage as MemStorage, Storage};
     use futures::TryStreamExt;
-    use tokio::{
-        io::{AsyncReadExt, AsyncWriteExt},
-        net::TcpListener,
-    };
+    use tokio::net::TcpListener;
     use tokio_stream::wrappers::TcpListenerStream;
 
     use super::Server;
@@ -39,19 +34,10 @@ mod tests {
         let listener = TcpListener::bind("127.0.0.1:0").await?;
         let local_addr = listener.local_addr()?;
         tokio::task::spawn(async move {
-            let j = MemJournal::default();
-            let stream = j.create_stream("DEFAULT").await.unwrap();
-            let s = MemStorage::default();
-            let bucket = s.create_bucket("DEFAULT").await.unwrap();
-            let manifest = mem::Manifest::default();
-            let kernel = mem::Kernel::init(stream, bucket, manifest).await.unwrap();
+            let kernel = mem::Kernel::open().await.unwrap();
             let server = Server::new(kernel);
-            let journal_server = JournalServer::new(j);
-            let storage_server = StorageServer::new(s);
             tonic::transport::Server::builder()
                 .add_service(server.into_service())
-                .add_service(journal_server.into_service())
-                .add_service(storage_server.into_service())
                 .serve_with_incoming(TcpListenerStream::new(listener))
                 .await
                 .unwrap();
@@ -59,40 +45,6 @@ mod tests {
 
         let url = format!("http://{}", local_addr);
         let kernel = grpc::Kernel::connect(&url).await?;
-
-        // For stream
-        let stream = kernel.stream().await?;
-        let ts = 31340128116183;
-        let event = Event {
-            ts: ts.into(),
-            data: vec![0, 1, 2],
-        };
-        stream.append_event(event.clone()).await?;
-        {
-            let mut events = stream.read_events(0.into()).await;
-            let got = events.try_next().await?.unwrap();
-            assert_eq!(got, vec![event]);
-        }
-        stream.release_events((ts + 1).into()).await?;
-        {
-            let mut events = stream.read_events(0.into()).await;
-            let got = events.try_next().await?.unwrap();
-            assert_eq!(got, vec![]);
-        }
-
-        // For bucket
-        let b = kernel.bucket().await?;
-        let mut w = b.new_sequential_writer("object").await?;
-        let buf = vec![0, 1, 2];
-        w.write(&buf).await?;
-        w.flush().await?;
-        w.shutdown().await?;
-        let mut r = b.new_sequential_reader("object").await?;
-        let mut got = Vec::new();
-        r.read_to_end(&mut got).await?;
-        assert_eq!(got, buf);
-
-        // For kernel
         let version = kernel.current_version().await?;
         assert_eq!(version.sequence, 0);
         assert_eq!(version.meta.len(), 0);
