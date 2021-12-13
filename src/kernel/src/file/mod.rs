473358411a00672a4fc3360ab333fd5358a18863
Richard <huachao.huang@gmail.com>
2021-12-13 18:25:10 +0800

    Address comment

--- src/kernel/src/file/mod.rs
+++ src/kernel/src/file/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! A [`Kernel`] implementation that stores everything in memory.
+//! A [`Kernel`] implementation that stores everything in local files.
 //!
 //! [`Kernel`]: crate::Kernel
 
