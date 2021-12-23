cde457d07a8cbc0ce6399b3d1ff6acf9a13ef7e8
tison <wander4096@gmail.com>
2021-12-23 23:15:50 +0800

    bin: support parse localhost
    Signed-off-by: tison <wander4096@gmail.com>

--- src/kernel/src/lib.rs
+++ src/kernel/src/lib.rs
@@ -49,6 +49,7 @@ pub type ResultStream<T> = Box<dyn futures::Stream<Item = Result<T>> + Send + Un
 pub use self::{
     error::{Error, Result},
     kernel::{Kernel, KernelUpdate},
+    manifest::Manifest,
     metadata::{Sequence, Version, VersionUpdate},
 };
 
