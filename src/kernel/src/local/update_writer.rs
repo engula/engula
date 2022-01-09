f4fdc413dcef9a1938d99b7737a3060ddce8fc41
Richard <huachao.huang@gmail.com>
2022-01-09 12:58:43 +0800

    kernel: execute some update operations

--- src/kernel/src/local/update_writer.rs
+++ src/kernel/src/local/update_writer.rs
@@ -17,24 +17,58 @@ use std::sync::{
     Arc,
 };
 
+use engula_journal::Journal;
+use engula_storage::Storage;
 use tokio::sync::broadcast::Sender;
 
 use crate::{async_trait, Error, KernelUpdate, Result, Sequence, UpdateEvent};
 
-pub struct UpdateWriter {
+pub struct UpdateWriter<J, S> {
+    journal: Arc<J>,
+    storage: Arc<S>,
     sequence: Arc<AtomicU64>,
     tx: Sender<UpdateEvent>,
 }
 
-impl UpdateWriter {
-    pub fn new(sequence: Arc<AtomicU64>, tx: Sender<UpdateEvent>) -> Self {
-        Self { sequence, tx }
+impl<J, S> UpdateWriter<J, S>
+where
+    J: Journal,
+    S: Storage,
+{
+    pub fn new(
+        journal: Arc<J>,
+        storage: Arc<S>,
+        sequence: Arc<AtomicU64>,
+        tx: Sender<UpdateEvent>,
+    ) -> Self {
+        Self {
+            journal,
+            storage,
+            sequence,
+            tx,
+        }
     }
 }
 
 #[async_trait]
-impl crate::UpdateWriter for UpdateWriter {
+impl<J, S> crate::UpdateWriter for UpdateWriter<J, S>
+where
+    J: Journal + Send + Sync,
+    S: Storage + Send + Sync,
+{
     async fn append(&mut self, update: KernelUpdate) -> Result<Sequence> {
+        for stream in &update.add_streams {
+            self.journal.create_stream(stream).await?;
+        }
+        for stream in &update.remove_streams {
+            self.journal.delete_stream(stream).await?;
+        }
+        for bucket in &update.add_buckets {
+            self.storage.create_bucket(bucket).await?;
+        }
+        for bucket in &update.remove_buckets {
+            self.storage.delete_bucket(bucket).await?;
+        }
         let sequence = self.sequence.fetch_add(1, Ordering::SeqCst) + 1;
         self.tx.send((sequence, update)).map_err(Error::unknown)?;
         Ok(sequence)
