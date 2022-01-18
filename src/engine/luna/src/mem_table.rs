6da001cafc38ab2d6c29368b6e878d9ae0860aff
Richard <huachao.huang@gmail.com>
2022-01-18 21:27:48 +0800

    luna: implement MemTable (#301)
    The `MemTableScanner` is left to another PR.

--- src/engine/luna/src/mem_table.rs
+++ src/engine/luna/src/mem_table.rs
@@ -12,12 +12,88 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::scan::Scan;
+use std::{collections::BTreeMap, ops::Bound::*, sync::Mutex};
 
-pub trait MemTable {
-    type Scanner: Scan;
+use crate::{
+    codec::{Timestamp, Value},
+    scan::Scan,
+    WriteBatch,
+};
 
-    fn scan(&self) -> Self::Scanner;
+pub struct MemTable {
+    inner: Mutex<Inner>,
+}
 
-    fn approximate_size(&self) -> usize;
+type ValueTree = BTreeMap<Timestamp, Value>;
+type Tree = BTreeMap<Vec<u8>, ValueTree>;
+
+struct Inner {
+    tree: Tree,
+    last_ts: Timestamp,
+    estimated_size: usize,
+}
+
+impl MemTable {
+    pub fn new(ts: Timestamp) -> Self {
+        let inner = Inner {
+            tree: Tree::new(),
+            last_ts: ts,
+            estimated_size: 0,
+        };
+        MemTable {
+            inner: Mutex::new(inner),
+        }
+    }
+
+    pub fn write(&self, batch: WriteBatch) {
+        let mut inner = self.inner.lock().unwrap();
+        inner.last_ts = batch.ts;
+        inner.estimated_size += batch.estimated_size;
+        for w in batch.writes {
+            let vtree = inner.tree.entry(w.0).or_insert_with(BTreeMap::new);
+            vtree.insert(batch.ts, w.1);
+        }
+    }
+
+    pub fn get(&self, ts: Timestamp, key: &[u8]) -> Option<Vec<u8>> {
+        let inner = self.inner.lock().unwrap();
+        inner.tree.get(key).and_then(|vtree| {
+            vtree
+                .range((Unbounded, Included(ts)))
+                .next_back()
+                .and_then(|x| x.1.clone())
+        })
+    }
+
+    pub fn scan(&self) -> MemTableScanner {
+        todo!();
+    }
+}
+
+pub struct MemTableScanner {}
+
+impl Scan for MemTableScanner {
+    fn seek_to_first(&mut self) {
+        todo!();
+    }
+
+    fn seek(&mut self) {
+        todo!();
+    }
+
+    fn next(&mut self) {
+        todo!();
+    }
+
+    fn valid(&self) -> bool {
+        todo!();
+    }
+
+    fn key(&self) -> &[u8] {
+        todo!();
+    }
+
+    fn value(&self) -> &[u8] {
+        todo!();
+    }
 }
