2bc167249f466cf6d9efa9223cc2c8618ac16674
Richard <huachao.huang@gmail.com>
2022-02-26 11:36:50 +0800

    object-engine: add table builder

--- src/object-engine/lsmstore/src/table/block_handle.rs
+++ src/object-engine/lsmstore/src/table/block_handle.rs
@@ -12,9 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod block_builder;
-mod block_handle;
-mod table_builder;
+use std::mem::size_of;
 
-pub use self::table_builder::{TableBuilder, TableBuilderOptions, TableDesc};
-use self::{block_builder::BlockBuilder, block_handle::BlockHandle};
+use bytes::BufMut;
+
+pub struct BlockHandle {
+    pub offset: u64,
+    pub length: u64,
+}
+
+impl BlockHandle {
+    pub fn encode_to<B: BufMut>(&self, buf: &mut B) {
+        buf.put_u64(self.offset);
+        buf.put_u64(self.length);
+    }
+
+    pub fn encode_to_vec(&self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(size_of::<u64>() * 2);
+        self.encode_to(&mut buf);
+        buf
+    }
+}
