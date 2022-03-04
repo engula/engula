d45e48102404ddb8d82a726dcc6eff0c8814a514
Richard <huachao.huang@gmail.com>
2022-03-04 15:41:24 +0800

    object-engine: add command to start the master

--- src/engula/engula/src/object_engine.rs
+++ src/engula/engula/src/object_engine.rs
@@ -14,6 +14,7 @@
 
 use anyhow::Result;
 use clap::Parser;
+use object_engine_master::{Master, Server};
 use tokio::net::TcpListener;
 use tokio_stream::wrappers::TcpListenerStream;
 use tracing::info;
@@ -33,32 +34,61 @@ impl Command {
 
 #[derive(Parser)]
 enum SubCommand {
-    Start(StartCommand),
+    Master(MasterCommand),
 }
 
 impl SubCommand {
     async fn run(self) -> Result<()> {
         match self {
-            SubCommand::Start(cmd) => cmd.run().await,
+            SubCommand::Master(cmd) => cmd.run().await,
         }
     }
 }
 
 #[derive(Parser)]
-struct StartCommand {
-    #[clap(long, default_value = "localhost:21716")]
-    addr: String,
+struct MasterCommand {
+    #[clap(subcommand)]
+    subcmd: MasterSubCommand,
 }
 
-impl StartCommand {
+impl MasterCommand {
+    pub async fn run(self) -> Result<()> {
+        self.subcmd.run().await?;
+        Ok(())
+    }
+}
+
+#[derive(Parser)]
+enum MasterSubCommand {
+    Start(MasterStartCommand),
+}
+
+impl MasterSubCommand {
+    async fn run(self) -> Result<()> {
+        match self {
+            MasterSubCommand::Start(cmd) => cmd.run().await,
+        }
+    }
+}
+
+#[derive(Parser)]
+struct MasterStartCommand {
+    #[clap(long, default_value = "localhost:21717")]
+    addr: String,
+    #[clap(long, default_value = "/tmp/object-engine")]
+    path: String,
+}
+
+impl MasterStartCommand {
     async fn run(self) -> Result<()> {
         let listener = TcpListener::bind(self.addr).await?;
         let addr = listener.local_addr()?;
-        info!(message = "The server is running at", %addr);
+        info!(message = "The master is running at", %addr);
 
-        let transactor = engula_transactor::Server::new().into_service();
+        let master = Master::open(self.path).await?;
+        let master_service = Server::new(master).into_service();
         tonic::transport::Server::builder()
-            .add_service(transactor)
+            .add_service(master_service)
             .serve_with_incoming(TcpListenerStream::new(listener))
             .await
             .map_err(|e| e.into())
