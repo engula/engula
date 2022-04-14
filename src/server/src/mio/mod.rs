03c2d7816a5a535536bf777be78d136bbcc1e3be
tison <wander4096@gmail.com>
2022-04-14 23:28:26 +0800

    support switch from drivers
    Signed-off-by: tison <wander4096@gmail.com>

--- src/server/src/mio/mod.rs
+++ src/server/src/mio/mod.rs
@@ -11,17 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#[derive(Debug, Copy, Clone)]
-pub enum DriverMode {
-    Tokio,
-    Mio,
-    #[cfg(target_os = "linux")]
-    Uio,
-}
-
-pub struct Config {
-    pub addr: String,
-    pub num_threads: usize,
-    pub driver_mode: DriverMode,
-}
