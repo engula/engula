250b400fae0946b6b3d129a8390f8ccf6f0dde39
tison <wander4096@gmail.com>
2022-06-01 18:57:16 +0800

    client: add node_client
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ src/client/src/node_client.rs
@@ -0,0 +1,36 @@
+// Copyright 2022 The Engula Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use engula_api::{server::v1::*, v1::*};
+use tonic::transport::Channel;
+
+#[derive(Debug, Clone)]
+pub struct Client {
+    client: node_client::NodeClient<Channel>,
+}
+
+impl Client {
+    pub async fn connect(addr: String) -> Result<Self, tonic::transport::Error> {
+        let addr = format!("http://{}", addr);
+        let client = node_client::NodeClient::connect(addr).await?;
+        Ok(Self { client })
+    }
+
+    pub async fn get_root(&self) -> Result<Vec<String>, tonic::Status> {
+        let mut client = self.client.clone();
+        let req = GetRootRequest::default();
+        let res = client.get_root(req).await?;
+        Ok(res.into_inner().addrs)
+    }
+}
