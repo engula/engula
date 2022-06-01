5b987c314dbe672956e67386c692d1fb846fee31
walter <w41ter.l@gmail.com>
2022-06-01 19:35:26 +0800

    api: add structured error message (#713)

--- src/api/src/lib.rs
+++ src/api/src/lib.rs
@@ -19,5 +19,122 @@ pub mod v1 {
 pub mod server {
     pub mod v1 {
         tonic::include_proto!("engula.server.v1");
+
+        impl GroupResponse {
+            #[inline]
+            pub fn new(response: group_response_union::Response) -> Self {
+                GroupResponse {
+                    response: Some(GroupResponseUnion {
+                        response: Some(response),
+                    }),
+                    error: None,
+                }
+            }
+
+            #[inline]
+            pub fn with_error(resp: group_response_union::Response, error: Error) -> Self {
+                GroupResponse {
+                    response: Some(GroupResponseUnion {
+                        response: Some(resp),
+                    }),
+                    error: Some(error),
+                }
+            }
+
+            #[inline]
+            pub fn error(error: Error) -> Self {
+                GroupResponse {
+                    response: None,
+                    error: Some(error),
+                }
+            }
+        }
+
+        impl ErrorDetail {
+            #[inline]
+            pub fn new(value: error_detail_union::Value) -> Self {
+                ErrorDetail {
+                    detail: Some(ErrorDetailUnion { value: Some(value) }),
+                    ..Default::default()
+                }
+            }
+
+            #[inline]
+            pub fn with_message(value: error_detail_union::Value, message: String) -> Self {
+                ErrorDetail {
+                    detail: Some(ErrorDetailUnion { value: Some(value) }),
+                    message,
+                }
+            }
+
+            #[inline]
+            pub fn not_leader(value: NotLeader) -> Self {
+                Self::new(error_detail_union::Value::NotLeader(value))
+            }
+
+            #[inline]
+            pub fn server_is_busy(value: ServerIsBusy) -> Self {
+                Self::new(error_detail_union::Value::ServerIsBusy(value))
+            }
+
+            #[inline]
+            pub fn not_match(value: NotMatch) -> Self {
+                Self::new(error_detail_union::Value::NotMatch(value))
+            }
+
+            #[inline]
+            pub fn group_not_found(value: GroupNotFound) -> Self {
+                Self::new(error_detail_union::Value::GroupNotFound(value))
+            }
+
+            #[inline]
+            pub fn status(code: i32, msg: impl Into<String>) -> Self {
+                Self::with_message(error_detail_union::Value::StatusCode(code), msg.into())
+            }
+        }
+
+        impl Error {
+            #[inline]
+            pub fn not_leader(group_id: u64, leader: Option<ReplicaDesc>) -> Self {
+                Self::with_detail_value(error_detail_union::Value::NotLeader(NotLeader {
+                    group_id,
+                    leader,
+                }))
+            }
+
+            #[inline]
+            pub fn server_is_busy() -> Self {
+                Self::with_detail_value(error_detail_union::Value::ServerIsBusy(ServerIsBusy {}))
+            }
+
+            #[inline]
+            pub fn not_match(group_id: u64, shard_id: u64) -> Self {
+                Self::with_detail_value(error_detail_union::Value::NotMatch(NotMatch {
+                    group_id,
+                    shard_id,
+                }))
+            }
+
+            #[inline]
+            pub fn group_not_found(group_id: u64) -> Self {
+                Self::with_detail_value(error_detail_union::Value::GroupNotFound(GroupNotFound {
+                    group_id,
+                }))
+            }
+
+            #[inline]
+            pub fn status(code: i32, msg: impl Into<String>) -> Self {
+                Error {
+                    details: vec![ErrorDetail::status(code, msg)],
+                }
+            }
+
+            #[inline]
+            pub fn with_detail_value(value: error_detail_union::Value) -> Self {
+                Error {
+                    details: vec![ErrorDetail::new(value)],
+                }
+            }
+        }
     }
 }
