6c13d034c9e0fe1bd1d837f405c4aabef809babe
walter <w41ter.l@gmail.com>
2022-06-09 11:04:40 +0800

    server: batch writes in shard (#733)

---
+++ src/server/src/node/replica/eval/cmd_batch_write.rs
@@ -0,0 +1,44 @@
+// Copyright 2022 The Engula Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use engula_api::server::v1::BatchWriteRequest;
+
+use crate::{
+    node::{group_engine::WriteBatch, GroupEngine},
+    serverpb::v1::{EvalResult, WriteBatchRep},
+    Result,
+};
+
+pub async fn batch_write(
+    group_engine: &GroupEngine,
+    shard_id: u64,
+    req: &BatchWriteRequest,
+) -> Result<Option<EvalResult>> {
+    if req.deletes.is_empty() && req.puts.is_empty() {
+        return Ok(None);
+    }
+
+    let mut wb = WriteBatch::default();
+    for del in &req.deletes {
+        group_engine.delete(&mut wb, shard_id, &del.key)?;
+    }
+    for put in &req.puts {
+        group_engine.put(&mut wb, shard_id, &put.key, &put.value)?;
+    }
+    Ok(Some(EvalResult {
+        batch: Some(WriteBatchRep {
+            data: wb.data().to_owned(),
+        }),
+        ..Default::default()
+    }))
+}
