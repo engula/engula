363a019971cfae58d59121a9bc648c14b4f09ca0
tison <wander4096@gmail.com>
2022-06-30 17:19:05 +0800

    not_leader -> not_root
    Signed-off-by: tison <wander4096@gmail.com>

--- src/client/src/root_client.rs
+++ src/client/src/root_client.rs
@@ -12,28 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, future::Future, sync::Arc};
 
 use engula_api::{server::v1::*, v1::*};
 use prost::{DecodeError, Message};
 use tokio::sync::Mutex;
-use tonic::{transport::Channel, Streaming};
+use tonic::{transport::Channel, Status, Streaming};
 
 use crate::NodeClient;
 
 #[derive(Debug, Clone)]
 pub struct Client {
-    ensemble: Vec<String>,
     client: Arc<Mutex<root_client::RootClient<Channel>>>,
 }
 
 async fn find_root_client(
-    ensemble: &[String],
+    candidates: &[String],
 ) -> Result<root_client::RootClient<Channel>, crate::Error> {
-    let node_client = find_node_client(ensemble).await?;
-    let root_candidates = node_client.get_root().await?;
     let mut errs = vec![];
-    for addr in root_candidates {
+    for addr in candidates {
         let root_addr = format!("http://{}", addr);
         match root_client::RootClient::connect(root_addr).await {
             Ok(client) => return Ok(client),
@@ -54,67 +51,57 @@ async fn find_node_client(ensemble: &[String]) -> Result<NodeClient, crate::Erro
     Err(crate::Error::MultiTransport(errs))
 }
 
-fn not_leader(err: Result<Error, DecodeError>) -> Option<NotLeader> {
-    if err.is_err() {
-        return None;
-    }
+fn not_root(err: Result<Error, DecodeError>) -> Option<NotRoot> {
+    let details = err.ok().map(|e| e.details)?;
 
-    let details = err.unwrap().details;
     if details.is_empty() {
         return None;
     }
 
     match &details[0].detail.as_ref().and_then(|u| u.value.clone()) {
-        Some(error_detail_union::Value::NotLeader(res)) => Some(res.clone()),
+        Some(error_detail_union::Value::NotRoot(res)) => Some(res.clone()),
         _ => None,
     }
 }
 
 impl Client {
     pub async fn connect(ensemble: Vec<String>) -> Result<Self, crate::Error> {
-        let client = find_root_client(ensemble.as_slice()).await?;
-        let client = Arc::new(Mutex::new(client));
-        Ok(Self { ensemble, client })
+        let node_client = find_node_client(ensemble.as_slice()).await?;
+        let candidates = node_client.get_root().await?;
+        let root_client = find_root_client(candidates.as_slice()).await?;
+        let client = Arc::new(Mutex::new(root_client));
+        Ok(Self { client })
     }
 
     pub async fn admin(&self, req: AdminRequest) -> Result<AdminResponse, crate::Error> {
-        let mut client = self.client.lock().await;
-        let res = match client.admin(req.clone()).await {
-            Ok(res) => res,
-            Err(status) => {
-                let _ = not_leader(Error::decode(status.details())).ok_or(status)?;
-                *client = find_root_client(self.ensemble.as_slice()).await?;
-                client.admin(req.clone()).await?
-            }
-        };
+        let res = self
+            .invoke(|mut client| {
+                let req = req.clone();
+                async move { client.admin(req).await }
+            })
+            .await?;
         Ok(res.into_inner())
     }
 
     pub async fn join_node(&self, req: JoinNodeRequest) -> Result<JoinNodeResponse, crate::Error> {
-        let mut client = self.client.lock().await;
-        let res = match client.join(req.clone()).await {
-            Ok(res) => res,
-            Err(status) => {
-                let _ = not_leader(Error::decode(status.details())).ok_or(status)?;
-                *client = find_root_client(self.ensemble.as_slice()).await?;
-                client.join(req.clone()).await?
-            }
-        };
+        let res = self
+            .invoke(|mut client| {
+                let req = req.clone();
+                async move { client.join(req).await }
+            })
+            .await?;
         Ok(res.into_inner())
     }
 
     // TODO removed once `watch` implemented
     pub async fn resolve(&self, node_id: u64) -> Result<ResolveNodeResponse, crate::Error> {
         let req = ResolveNodeRequest { node_id };
-        let mut client = self.client.lock().await;
-        let res = match client.resolve(req.clone()).await {
-            Ok(res) => res,
-            Err(status) => {
-                let _ = not_leader(Error::decode(status.details())).ok_or(status)?;
-                *client = find_root_client(self.ensemble.as_slice()).await?;
-                client.resolve(req.clone()).await?
-            }
-        };
+        let res = self
+            .invoke(|mut client| {
+                let req = req.clone();
+                async move { client.resolve(req).await }
+            })
+            .await?;
         Ok(res.into_inner())
     }
 
@@ -123,16 +110,30 @@ impl Client {
         cur_group_epochs: HashMap<u64, u64>,
     ) -> Result<Streaming<WatchResponse>, crate::Error> {
         let req = WatchRequest { cur_group_epochs };
-        let mut client = self.client.lock().await;
-        let res = match client.watch(req.clone()).await {
-            Ok(res) => res,
-            Err(status) => {
-                let _ = not_leader(Error::decode(status.details())).ok_or(status)?;
-                *client = find_root_client(self.ensemble.as_slice()).await?;
-                client.watch(req.clone()).await?
-            }
-        };
+        let res = self
+            .invoke(|mut client| {
+                let req = req.clone();
+                async move { client.watch(req).await }
+            })
+            .await?;
         Ok(res.into_inner())
+    }
+
+    async fn invoke<F, O, V>(&self, op: F) -> Result<V, crate::Error>
+    where
+        F: Fn(root_client::RootClient<Channel>) -> O,
+        O: Future<Output = Result<V, Status>>,
+    {
+        let mut client = self.client.lock().await;
+        match op(client.clone()).await {
+            Ok(res) => Ok(res),
+            Err(status) => {
+                let err = not_root(Error::decode(status.details())).ok_or(status)?;
+                let candidates = err.root;
+                *client = find_root_client(candidates.as_slice()).await?;
+                Ok(op(client.clone()).await?)
+            }
+        }
     }
 }
 
