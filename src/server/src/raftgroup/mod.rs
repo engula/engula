861496eb2daa1478c77a0e62de4dc0f6effd862e
w41ter <w41ter.l@gmail.com>
2022-08-03 17:54:51 +0800

    server: fix removing learners

--- src/server/src/raftgroup/mod.rs
+++ src/server/src/raftgroup/mod.rs
@@ -227,6 +227,7 @@ pub fn conf_state_from_group_descriptor(desc: &GroupDesc) -> ConfState {
             }
             ReplicaRole::Learner => {
                 cs.learners.push(replica.id);
+                cs.learners_next.push(replica.id);
             }
             ReplicaRole::IncomingVoter => {
                 in_joint = true;
