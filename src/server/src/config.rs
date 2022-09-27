afa4dca6a7b1e2ef22ac87846c950beffd8575f0
walter <w41ter.l@gmail.com>
2022-09-27 10:26:36 +0800

    server: set cf options from config (#1085)

--- src/server/src/config.rs
+++ src/server/src/config.rs
@@ -94,6 +94,66 @@ pub struct DbConfig {
     pub rate_limiter_auto_tuned: bool,
 }
 
+impl DbConfig {
+    pub fn to_options(&self) -> rocksdb::Options {
+        use rocksdb::{BlockBasedIndexType, BlockBasedOptions, Cache, Options};
+
+        let cfg = self;
+
+        let mut opts = Options::default();
+        opts.create_if_missing(true);
+        opts.create_missing_column_families(true);
+
+        opts.set_max_background_jobs(cfg.max_background_jobs);
+        opts.set_max_subcompactions(cfg.max_sub_compactions);
+        opts.set_max_manifest_file_size(cfg.max_manifest_file_size);
+        opts.set_bytes_per_sync(cfg.bytes_per_sync);
+        opts.set_compaction_readahead_size(cfg.compaction_readahead_size);
+        opts.set_use_direct_reads(cfg.use_direct_read);
+        opts.set_use_direct_io_for_flush_and_compaction(cfg.use_direct_io_for_flush_and_compaction);
+        opts.set_avoid_unnecessary_blocking_io(cfg.avoid_unnecessary_blocking_io);
+
+        opts.set_write_buffer_size(cfg.write_buffer_size);
+        opts.set_max_write_buffer_number(cfg.max_write_buffer_number);
+        opts.set_min_write_buffer_number_to_merge(cfg.min_write_buffer_number_to_merge);
+
+        opts.set_num_levels(cfg.num_levels);
+        opts.set_compression_per_level(&cfg.compression_per_level);
+
+        opts.set_level_zero_file_num_compaction_trigger(cfg.level0_file_num_compaction_trigger);
+        opts.set_target_file_size_base(cfg.target_file_size_base);
+        opts.set_max_bytes_for_level_base(cfg.max_bytes_for_level_base);
+        opts.set_max_bytes_for_level_multiplier(cfg.max_bytes_for_level_multiplier);
+        opts.set_max_compaction_bytes(cfg.max_compaction_bytes);
+        opts.set_level_compaction_dynamic_level_bytes(true);
+
+        opts.set_level_zero_slowdown_writes_trigger(cfg.level0_slowdown_writes_trigger);
+        opts.set_level_zero_stop_writes_trigger(cfg.level0_slowdown_writes_trigger);
+        opts.set_soft_pending_compaction_bytes_limit(cfg.soft_pending_compaction_bytes_limit);
+        opts.set_hard_pending_compaction_bytes_limit(cfg.hard_pending_compaction_bytes_limit);
+
+        opts.set_auto_tuned_ratelimiter(
+            cfg.rate_limiter_bytes_per_sec,
+            cfg.rate_limiter_refill_period,
+            10,
+            cfg.rate_limiter_auto_tuned,
+        );
+
+        let cache = Cache::new_lru_cache(cfg.block_cache_size).expect("new lrc cache");
+
+        let mut blk_opts = BlockBasedOptions::default();
+        blk_opts.set_index_type(BlockBasedIndexType::TwoLevelIndexSearch);
+        blk_opts.set_block_size(cfg.block_size);
+        blk_opts.set_block_cache(&cache);
+        blk_opts.set_cache_index_and_filter_blocks(true);
+        blk_opts.set_bloom_filter(10.0, false);
+        opts.set_block_based_table_factory(&blk_opts);
+
+        opts.create_missing_column_families(true);
+        opts
+    }
+}
+
 impl Default for DbConfig {
     fn default() -> Self {
         DbConfig {
--- src/server/src/node/engine/mod.rs
+++ src/server/src/node/engine/mod.rs
@@ -15,6 +15,8 @@
 mod group;
 mod state;
 
+use std::{path::Path, sync::Arc};
+
 pub use self::{
     group::{
         EngineConfig, GroupEngine, RawIterator, Snapshot, SnapshotMode, WriteBatch, WriteStates,
@@ -22,3 +24,98 @@ pub use self::{
     },
     state::StateEngine,
 };
+
+type Result<T> = std::result::Result<T, rocksdb::Error>;
+
+pub struct RawDb {
+    pub options: rocksdb::Options,
+    pub db: rocksdb::DB,
+}
+
+impl RawDb {
+    #[inline]
+    pub fn cf_handle(&self, name: &str) -> Option<Arc<rocksdb::BoundColumnFamily>> {
+        self.db.cf_handle(name)
+    }
+
+    #[inline]
+    pub fn create_cf<N: AsRef<str>>(&self, name: N) -> Result<()> {
+        self.db.create_cf(name, &self.options)
+    }
+
+    #[inline]
+    pub fn drop_cf(&self, name: &str) -> Result<()> {
+        self.db.drop_cf(name)
+    }
+
+    #[inline]
+    pub fn flush_cf(&self, cf: &impl rocksdb::AsColumnFamilyRef) -> Result<()> {
+        self.db.flush_cf(cf)
+    }
+
+    #[inline]
+    pub fn write_opt(
+        &self,
+        batch: rocksdb::WriteBatch,
+        writeopts: &rocksdb::WriteOptions,
+    ) -> Result<()> {
+        self.db.write_opt(batch, writeopts)
+    }
+
+    #[inline]
+    pub fn get_pinned_cf<K: AsRef<[u8]>>(
+        &self,
+        cf: &impl rocksdb::AsColumnFamilyRef,
+        key: K,
+    ) -> Result<Option<rocksdb::DBPinnableSlice>> {
+        self.db.get_pinned_cf(cf, key)
+    }
+
+    #[inline]
+    pub fn get_pinned_cf_opt<K: AsRef<[u8]>>(
+        &self,
+        cf: &impl rocksdb::AsColumnFamilyRef,
+        key: K,
+        readopts: &rocksdb::ReadOptions,
+    ) -> Result<Option<rocksdb::DBPinnableSlice>> {
+        self.db.get_pinned_cf_opt(cf, key, readopts)
+    }
+
+    #[inline]
+    pub fn iterator_cf<'a: 'b, 'b>(
+        &'a self,
+        cf_handle: &impl rocksdb::AsColumnFamilyRef,
+        mode: rocksdb::IteratorMode,
+    ) -> rocksdb::DBIteratorWithThreadMode<'b, rocksdb::DB> {
+        self.db.iterator_cf(cf_handle, mode)
+    }
+
+    #[inline]
+    pub fn iterator_cf_opt<'a: 'b, 'b>(
+        &'a self,
+        cf_handle: &impl rocksdb::AsColumnFamilyRef,
+        readopts: rocksdb::ReadOptions,
+        mode: rocksdb::IteratorMode,
+    ) -> rocksdb::DBIteratorWithThreadMode<'b, rocksdb::DB> {
+        self.db.iterator_cf_opt(cf_handle, readopts, mode)
+    }
+
+    #[inline]
+    pub fn ingest_external_file_opts<P: AsRef<Path>>(
+        &self,
+        opts: &rocksdb::IngestExternalFileOptions,
+        paths: Vec<P>,
+    ) -> Result<()> {
+        self.db.ingest_external_file_opts(opts, paths)
+    }
+
+    #[inline]
+    pub fn ingest_external_file_cf_opts<P: AsRef<Path>>(
+        &self,
+        cf: &impl rocksdb::AsColumnFamilyRef,
+        opts: &rocksdb::IngestExternalFileOptions,
+        paths: Vec<P>,
+    ) -> Result<()> {
+        self.db.ingest_external_file_cf_opts(cf, opts, paths)
+    }
+}
